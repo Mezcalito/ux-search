6eb961f9985171d8d8f75aa6cdf5b69cf72f2b0d
Alexandre Petrone <alexandre.petrone@mezcalito.fr>
2025-07-16 11:02:46 +0200

    Reset current page after queryString, activeSort or activeHitsPerPage update (#19)
    Fix #15

--- src/Twig/Components/Layout.php
+++ src/Twig/Components/Layout.php
@@ -36,7 +36,11 @@ class Layout
     use ComponentToolsTrait;
     use DefaultActionTrait;
 
-    #[LiveProp(writable: ['queryString', 'activeSort', 'activeHitsPerPage'], useSerializerForHydration: true)]
+    #[LiveProp(
+        writable: ['queryString', 'activeSort', 'activeHitsPerPage'],
+        useSerializerForHydration: true,
+        onUpdated: ['queryString' => 'resetCurrentPage', 'activeSort' => 'resetCurrentPage', 'activeHitsPerPage' => 'resetCurrentPage']
+    )]
     public ?Query $query = null;
 
     #[LiveProp]
@@ -87,6 +91,11 @@ class Layout
     public function changeCurrentPage(#[LiveArg] int $page): void
     {
         $this->query->setCurrentPage($page);
+    }
+
+    public function resetCurrentPage(string|int|null $previousValue): void
+    {
+        $this->query->setCurrentPage(1);
     }
 
     #[LiveAction]
